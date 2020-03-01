c852cf794a40361c4ca1fb9dac49469a26d3293b
hadian <hadian.online@gmail.com>
2020-03-01 20:17:01 +0700

    Main - Implementation Navigation At Main

--- app/src/main/java/xyz/tasikcode/tasikcodeapp/MainActivity.kt
+++ app/src/main/java/xyz/tasikcode/tasikcodeapp/MainActivity.kt
@@ -2,11 +2,21 @@ package xyz.tasikcode.tasikcodeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val navController = this.findNavController(R.id.navHostFragmentMain)
+        NavigationUI.setupWithNavController(navBottomMain, navController)
     }
 }
